d548806c62e6f140c2dfb7f09a7ced9015561702
Katlego Komane <113278179+CynicsTech@users.noreply.github.com>
2023-01-15 06:27:58 +0200

    55

--- math.js
+++ math.js
@@ -4,5 +4,13 @@ const num1 = Math.ceil(Math.random() * 100);
 const num2 = Math.ceil(Math.random() * 5);
 
 const question = document.getElementById("question");
+const form = document.getElementById("form");
 
 question.innerText = `What is the product of ${num1} and ${num2}?`;
+
+const correctAns = num1 * num2;
+
+form.addEventListener("submit", () => {
+
+    
+});
