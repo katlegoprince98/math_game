be51b202589178ba52132f98a0e38ec156bab756
Katlego Komane <113278179+CynicsTech@users.noreply.github.com>
2023-01-15 06:23:19 +0200

    pro

--- math.js
+++ math.js
@@ -2,3 +2,7 @@
 
 const num1 = Math.ceil(Math.random() * 10);
 const num2 = Math.ceil(Math.random() * 10);
+
+const question = document.getElementById("question");
+
+question.innerText = `What is the product of ${num1} and ${num1}?`;
